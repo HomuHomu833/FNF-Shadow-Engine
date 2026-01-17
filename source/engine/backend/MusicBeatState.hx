5ae9dea43f96025c5ef0c0bb4730d70b9203edbe
Karim Akra <karimakra512@gmail.com>
2026-01-17 20:21:53 +0200

    Added IMusicState

--- source/engine/backend/MusicBeatState.hx
+++ source/engine/backend/MusicBeatState.hx
@@ -12,7 +12,7 @@ import flixel.util.FlxSave;
 import backend.PsychCamera;
 import haxe.io.Path;
 
-class MusicBeatState extends #if MODCHARTS_ALLOWED ModchartMusicBeatState #else FlxUIState #end
+class MusicBeatState extends #if MODCHARTS_ALLOWED ModchartMusicBeatState #else FlxUIState #end implements IMusicState
 {
 	private var curSection:Int = 0;
 	private var stepsToDo:Int = 0;
@@ -559,7 +559,7 @@ class MusicBeatState extends #if MODCHARTS_ALLOWED ModchartMusicBeatState #else 
 				func(stage);
 	}
 
-	function getBeatsOnSection()
+	public function getBeatsOnSection()
 	{
 		var val:Null<Float> = 4;
 		if (PlayState.SONG != null && PlayState.SONG.notes[curSection] != null)
